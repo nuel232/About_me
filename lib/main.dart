84865effdb167319ab0f818ee89a365091bf1e75
nuel232 <nwankwoala3@gmail.com>
2025-11-11 16:56:22 +0100

    Initial commit of main application file with basic structure and theme provider integration

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:about_me/pages/home_screen.dart';
+import 'package:about_me/theme/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(1920, 1080),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return Consumer<ThemeProvider>(
+          builder: (context, themeProvider, child) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Kelechi - Full Stack Developer',
+              theme: Provider.of<ThemeProvider>(context).themeData,
+              home: const HomeScreen(),
+            );
+          },
+        );
+      },
+    );
+  }
+}
